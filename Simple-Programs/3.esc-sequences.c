6c6bab1143a151bda76727f9f7027b4b0bd1c512
Syahmi Fauzi <brosyahmifauzi@gmail.com>
2017-02-05 00:52:39 +0800

    Initial commit

--- Simple-Programs/3.esc-sequences.c
+++ Simple-Programs/3.esc-sequences.c
@@ -7,18 +7,18 @@ int main()
 	/* Escape Sequences always begin with a backslash..
 	 * ------------------------------------------------
 	 * Escape Sequence			Character
-	 *  		 a					Bell (speaker beeps)
-	 *			 b					Backspace (non-erase)
-	 *			 f					Form feed/clear screen
-	 *			 n					New line
-	 *			 r					Carriage Return
-	 *			 t					Tab
-	 *			 v					Vertical tab
-	 *			 \					Backslash
-	 *			 ?					Question mark
-	 *			 ‘					Single quote
-	 * 			 "				 	Double quote
-   */
+	 *	a			Bell (speaker beeps)
+	 *	b			Backspace (non-erase)
+	 *	f			Form feed/clear screen
+	 *	n			New line
+	 *	r			Carriage Return
+	 *	t			Tab
+	 *	v			Vertical tab
+	 *	\			Backslash
+	 *	?			Question mark
+	 *	‘			Single quote
+	 *	"			Double quote
+	 */
 
 	return 0;
 }
--- Simple-Programs/6.garbage-val.c
+++ Simple-Programs/6.garbage-val.c
@@ -3,11 +3,11 @@
 int main()
 {
 	int x,num1;
-	printf("The value of x is %d\n", x); 						/* if it prints non-zero number, it is called garbage value */
+	printf("The value of x is %d\n", x);					/* if it prints non-zero number, it is called garbage value */
 	printf("The value of num1 is %d\n", num1);
 	printf("The sum of x and num1 = %d\n", x+num1);
 
-	// int num3 = 100;															/* try uncomment num3 and recompile this file and see if there any difference */
+	// int num3 = 100;					/* try uncomment num3 and recompile this file and see if there any difference */
 
 	return 0;
 }
