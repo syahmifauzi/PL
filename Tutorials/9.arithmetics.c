b3d38f5546b9c6fc3787e5cf36557aac34e80531
Syahmi Fauzi <brosyahmifauzi@gmail.com>
2017-02-08 00:55:16 +0800

    add problems & tutorials

--- Tutorials/9.arithmetics.c
+++ Tutorials/9.arithmetics.c
@@ -15,13 +15,13 @@ int main() {
 	printf("\nAddition is %d", add);
 
 	sub = num1 - num2;                       /* subtraction */
-  printf("\nSubtraction is %d", sub);
+	printf("\nSubtraction is %d", sub);
 
 	mul = num1 * num2;                       /* multiplication */
-  printf("\nMultiplication is %d", mul);
+	printf("\nMultiplication is %d", mul);
 
 	div = num1 / num2;                       /* division */
-  printf("\nDivision is %d", div);
+	printf("\nDivision is %d", div);
 
 	mod = num1 % num2;                       /* modulus */
 	printf("\nModulus is %d\n\n", mod);
